50608ca1e51bb7963226562f47a06722e6e7c715
Michael Bahari <bahayana@gmail.com>
2019-03-22 10:20:51 +1100

    added table

---
+++ action.js
@@ -0,0 +1,7 @@
+function openNav(myNav) {
+    document.getElementById(myNav).style.display = "block";
+ }
+ 
+ function closeNav(myNav) {
+ document.getElementById(myNav).style.display = "none";
+ }
